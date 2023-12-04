b50d2cc2b09e3cbf432712d49d2846148d26213b
westerovs@mail.ru <westerovs@mail.ru>
2023-12-04 03:37:56 +0300

    flip is working!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,59 +1,69 @@
 const DATA_CARDS = [
   {
-    question: `вопрос`,
-    answer: `ответ`,
+    question: `вопрос: `,
+    answer: `ответ: `,
   }
 ]
-
-
 
 class Card {
   constructor() {
     this.isShowFace = true // по умолчанию лицо открыто
 
     this.card = document.querySelector('.card')
+    this.cardText = document.querySelector('.card__text')
     this.card.addEventListener('pointerdown', this.onHandlerDown)
-    // this.card.addEventListener('transitionend', this.flipCard)
   }
 
+  // Обработчик события нажатия
   onHandlerDown = (e) => {
     e.preventDefault()
 
     this.flipCard()
-    this.disabledCard()
+    this.toggleCardInteraction()
   }
 
+  // Метод для переворота карты
   flipCard = () => {
     if (this.isShowFace) {
-
+      this.animateCardFlip(false, 'card-answer')
+      this.showText(false)
     } else {
-
+      this.animateCardFlip(true, 'card-answer')
+      this.showText(true)
     }
 
-    // this.card.style.transform = 'scaleX(0)'
-    // this.card.style.transform = 'scaleX(1)'
-    // this.card.classList.add('card-answer')
-    // this.unDisabledCard()
-    // this.isShowFace = !this.isShowFace
+    this.isShowFace = !this.isShowFace
   }
 
-
-  showFace = () => {
-
+  // Анимация переворота карты
+  animateCardFlip = (showFace, className) => {
+    gsap.timeline()
+      .to(this.card, { scaleX: 0 })
+      .call(() => {
+        // Добавление или удаление класса
+        if (showFace) {
+          this.card.classList.remove(className)
+        } else {
+          this.card.classList.add(className)
+        }
+      })
+      .to(this.card, { scaleX: 1 })
+      .call(() => {
+        this.toggleCardInteraction() // Восстановление взаимодействия с картой
+      })
   }
 
-  showShirt = () => {
-
+  // Метод для временного отключения взаимодействия с картой
+  toggleCardInteraction = () => {
+    this.card.style.pointerEvents = (this.card.style.pointerEvents === 'none') ? 'auto' : 'none'
   }
 
-  disabledCard = () => {
-    this.card.style.pointerEvents = 'none'
+  // Метод для отображения текста
+  showText = (showFace) => {
+    const cardData = DATA_CARDS[0]
+    const textToShow = showFace ? cardData.question : cardData.answer
+    this.cardText.textContent = textToShow
   }
-
-  unDisabledCard = () => {
-    this.card.style.pointerEvents = 'auto'
-  }
-
 }
 
 new Card()
